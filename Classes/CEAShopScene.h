eff82adfd23538b9bb8d5f367a7a08bd18d6420c
Bin WANG <netmad@163.com>
2015-11-15 19:49:47 +0800

    add welcome scene and shop scene

---
+++ Classes/CEAShopScene.h
@@ -0,0 +1,14 @@
+//
+//  CEAShopScene.hpp
+//  cross-elimination-army
+//
+//  Created by wangbin on 15/11/15.
+//
+//
+
+#ifndef CEAShopScene_hpp
+#define CEAShopScene_hpp
+
+#include <stdio.h>
+
+#endif /* CEAShopScene_hpp */
