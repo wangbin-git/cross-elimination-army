b65f0f539e96432f2825fbf8faa33bd2ac36f279
Bin WANG <netmad@163.com>
2015-12-25 16:29:51 +0800

    modify AchievementsScene

---
+++ Classes/CEAAchievementScene.h
@@ -0,0 +1,13 @@
+//
+//  CEAAchievementScene.h
+//  cross-elimination-army
+//
+//  Created by wangbin on 15/11/15.
+//
+//
+
+#ifndef CEAAchievementScene_h
+#define CEAAchievementScene_h
+
+
+#endif /* CEAAchievementScene_h */
