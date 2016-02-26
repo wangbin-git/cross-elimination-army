e626ee281a2b32dc385bea34251f4473d8f0ea32
Bin WANG <netmad@163.com>
2016-02-26 22:33:20 +0800

    add CEAOverGameWin

---
+++ Classes/Model/CEAOverGameWin.cpp
@@ -0,0 +1,13 @@
+//
+//  CEAOverGameWin.cpp
+//  cross-elimination-army
+//
+//  Created by wangbin on 16/2/26.
+//
+//
+
+#include <stdio.h>
+#include "CEAOverGameWin.h"
+
+USING_NS_CC;
+
---
+++ Classes/Model/CEAOverGameWin.h
@@ -0,0 +1,22 @@
+//
+//  CEAOverGameWin.h
+//  cross-elimination-army
+//
+//  Created by wangbin on 16/2/26.
+//
+//
+
+#ifndef CEAOverGameWin_h
+#define CEAOverGameWin_h
+
+
+
+class CEAOverGameWin : public cocos2d::Sprite {
+    
+public:
+    
+public:
+    
+};
+
+#endif /* CEAOverGameWin_h */
