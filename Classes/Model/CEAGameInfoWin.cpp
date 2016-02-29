6fb1226858e5bdd0b2cf96c2f283af4094ddbf7c
Bin WANG <netmad@163.com>
2016-02-29 19:21:20 +0800

    rename OverGameWin to GameInfoWin

---
+++ Classes/Model/CEAGameInfoWin.cpp
@@ -0,0 +1,13 @@
+//
+//  CEAOverGameWin.cpp
+//  cross-elimination-army
+//
+//  Created by wangbin on 16/2/26.
+//
+//
+
+#include <stdio.h>
+#include "CEAGameInfoWin.h"
+
+USING_NS_CC;
+
---
+++ Classes/Model/CEAGameInfoWin.h
@@ -0,0 +1,22 @@
+//
+//  CEAGameInfoWin_h
+//  cross-elimination-army
+//
+//  Created by wangbin on 16/2/26.
+//
+//
+
+#ifndef CEAGameInfoWin_h
+#define CEAGameInfoWin_h
+
+
+
+class CEAGameInfoWin : public cocos2d::Sprite {
+    
+public:
+    
+public:
+    
+};
+
+#endif /* CEAGameInfoWin_h */
